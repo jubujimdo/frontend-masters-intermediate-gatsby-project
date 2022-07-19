1beee1ce13ad1c4fcf2985b4499e5fdc5eecf28b
Julian Buchholz <julian.buchholz@jimdo.com>
2022-07-19 10:25:11 +0200

    add: authors as nodes

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,26 @@
+const authors=require('./src/data/authors.json')
+const books=require('./src/data/books.json')
+
+exports.sourceNodes = ({actions, createNodeId, createContentDigest})=>{
+
+    const {createNode}=actions;
+
+    authors.forEach((author)=>{
+        createNode({
+            ...author,
+            id: createNodeId(`author-${author.slug}`),
+            parent: null,
+            children: [], 
+            internal: {
+                type: 'Author',
+                content: JSON.stringify(author),
+                contentDigest: createContentDigest(author)
+            }
+        })
+    }
+    )
+};
+
 exports.createPages = ({actions}) => {
     const {createPage} = actions;
     
