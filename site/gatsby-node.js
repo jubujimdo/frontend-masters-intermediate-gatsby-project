b884e21d292079a81b18e17ebc5ef367dbd2e0ba
Julian Buchholz <julian.buchholz@jimdo.com>
2022-07-19 16:44:21 +0200

    add: book covers from external sources

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch');
+const {createRemoteFileNode} = require('gatsby-source-filesystem');
+
 const authors=require('./src/data/authors.json')
 const books=require('./src/data/books.json')
 
@@ -61,12 +64,50 @@ exports.createPages = ({actions}) => {
     })
 }
 
-exports.createResolvers = ({createResolvers}) => {
-    const resolver = {
+exports.createResolvers = ({
+    actions,
+    cache,
+    createNodeId,
+    store,
+    reporter,
+    createResolvers}) => {
+
+    const {createNode} = actions;
+
+    const resolvers = {
         Book: {
             buyLink: {
                 type: 'String',
                 resolve: (source) => `https://www.powells.com/searchresults?keyword=${source.isbn}`,
+            },
+            cover: {
+                type: 'File',
+                resolve: async (source) => {
+                    const response = await fetch(`https://openlibrary.org/isbn/${source.isbn}.json`);
+
+                    if(!response.ok){
+                        reporter.warn(
+                            `Error loading details about ${source.name} - got ${response.status} ${response.statusText}`
+                        );
+                        return null
+                    }
+
+                    const {covers} = await response.json()
+
+                    if (covers.length) {
+                        return createRemoteFileNode({
+                            url: `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`,
+                            store,
+                            cache,
+                            createNode,
+                            createNodeId,
+                            reporter
+                        });
+                    }
+                    else{
+                        return null
+                    }
+                }
             }
         }
     }
